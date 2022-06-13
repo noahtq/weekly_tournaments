37f7c20fb0ee81835ec9291d4f447dc4092b280d
noahtq <vfx.noaht@gmail.com>
2022-06-13 12:27:47 -0500

    fix bug where times for events were not displaying correctly

--- events/models.py
+++ events/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    type = models.CharField(max_length=50)
+    description = models.TextField(default='', blank=True)
+    location = models.CharField(max_length=100)
+    event_datetime = models.DateTimeField(default=timezone.now)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    registered_users = models.ManyToManyField(User, related_name="registeredusers", blank=True)
+
+    def __str__(self):
+        return self.title
