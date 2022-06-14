1ddba5e4e33cec2cda780be0d40622a26f22c2bb
noahtq <vfx.noaht@gmail.com>
2022-06-14 12:11:36 -0500

    Add styling for update profile page, add individual images for different form pages, create seperate profile page and edit page

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from enum import unique
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.core.validators import RegexValidator
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,9 +10,21 @@ class UserRegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
 
-    # phone_number_regex = RegexValidator(regex=r'^\+?1?\d{8,15}$')
-    # phone_number = forms.CharField(validators=[phone_number_regex], max_length=16, unique=True)
-
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 800 or img.width > 800:
+            output_size = (800, 800)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
