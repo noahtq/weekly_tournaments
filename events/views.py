3c1e72627a5a09f1fe57414567a80d44884c6e10
noahtq <vfx.noaht@gmail.com>
2022-06-16 16:47:41 -0500

    Delete unused events forms file, add title of event to event register page

--- events/views.py
+++ events/views.py
@@ -12,18 +12,6 @@ class StaffRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
         return self.request.user.is_staff
 
 
-class NotRegisteredMixin(LoginRequiredMixin, UserPassesTestMixin):
-    
-    def test_func(self):
-        users_registered = Event.objects.filter(id=3).first().registered_users.all()
-        not_registered = True
-        for user in users_registered:
-            if user.id == self.request.user.id:
-                not_registered = False
-        return not_registered
-
-    
-
 def home(request):
     context = {
         'event': Event.objects.all().first(),
@@ -40,12 +28,6 @@ class EventListView(ListView):
 
 class EventDetailView(DetailView):
     model = Event
-
-
-# class EventRegisterView(NotRegisteredMixin, UpdateView):
-#     model = Event
-#     template_name = 'events/event_register.html'
-#     fields = ['registered_users']
 
 
 class EventCreateView(StaffRequiredMixin, CreateView):
@@ -74,16 +56,21 @@ class EventDeleteView(StaffRequiredMixin, DeleteView):
 def about(request):
     return render(request, 'events/about.html', {'title': 'About'})
 
+
 def EventRegisterView(request):
     event_pk = request.GET.get('event')
+    event = Event.objects.get(pk=event_pk)
+    user = request.user
     if request.method == 'POST':
-        user = request.user
-        event = Event.objects.get(pk=event_pk)
 
         event.registered_users.add(user)
 
         messages.success(request, f'You have been registered for {event.title}')
         return redirect('/')
 
-    return render(request, 'events/event_register.html')
+    context = {
+        'event': event
+    }
+
+    return render(request, 'events/event_register.html', context)
 
