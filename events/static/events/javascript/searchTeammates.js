3235e3541e612b4f684f375aff755e952ec8ac9e
noahtq <vfx.noaht@gmail.com>
2022-06-23 12:04:39 -0500

    fix border when only one search element is visible

--- events/static/events/javascript/searchTeammates.js
+++ events/static/events/javascript/searchTeammates.js
@@ -2,6 +2,22 @@ const searchNode = document.getElementById('teammate-searchbar');
 const listElements = document.querySelectorAll('li[class=select-item]')
 
 function updateList(e) {
+
+    //add border to list elements
+    for (let i = 0; i < listElements.length; i++) {
+        if(i > 1) {
+            listElements[i].style.borderTop = '1px solid var(--base)';
+        }
+    }
+
+    if(e.target.value.trim() == '') {
+        for (let listNode of listElements) {
+            listNode.style.display = 'list-item';
+        }
+    }
+
+    const visibleNodes = [];
+
     for (let listNode of listElements) {
         let show = false;
         childLabel = listNode.children[0];
@@ -9,10 +25,18 @@ function updateList(e) {
         for (let grandChild of grandchildText) {
             const innerText = grandChild.innerHTML.toLowerCase()
             if(innerText.includes(e.target.value.toLowerCase())) {
+                visibleNodes.push(listNode);
                 show = true;
             }
         }
         show ? listNode.style.display = 'list-item' : listNode.style.display = 'none';
+    }
+
+    //remove border if there is only one list element visible
+    if (visibleNodes.length <= 2) {
+        for (let listNode of listElements) {
+            listNode.style.borderTop = '0px solid black';
+        }
     }
 }
 
