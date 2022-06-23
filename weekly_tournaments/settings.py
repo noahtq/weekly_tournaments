edc9ab2a8a31bdde06c34c6eb14ba635a4a0eeb8
noahtq <vfx.noaht@gmail.com>
2022-06-23 06:40:13 -0500

    finish making home page responsive

--- weekly_tournaments/settings.py
+++ weekly_tournaments/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b&a73t#o1ya)^*8+!8^cqtp(v&bud*w3sop8%6fu*iz160$9p3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost' ,'0fea-73-5-168-121.ngrok.io']
+ALLOWED_HOSTS = ['localhost' ,'5076-23-88-148-124.ngrok.io']
 
 
 # Application definition
